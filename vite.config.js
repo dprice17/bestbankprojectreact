4c59ac933a4cf0b205d1517f86b0f5d7a6371c5f
dprice17 <109916870+dprice17@users.noreply.github.com>
2023-07-03 10:33:39 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import fs from "fs/promises";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/bestbank123/",
   plugins: [react()],
-  base: "/bestbankprojectreact",
+  build: {
+    rollupOptions: {
+      input: "/src/main.jsx",
+      external: ["uuid"],
+    },
+  },
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        {
+          name: "load-js-files-as-jsx",
+          setup(build) {
+            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
+              loader: "jsx",
+              contents: await fs.readFile(args.path, "utf8"),
+            }));
+          },
+        },
+      ],
+    },
+  },
 });
