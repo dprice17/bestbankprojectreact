7778034b8476071083df1b3590a5316be2415d0b
dprice17 <109916870+dprice17@users.noreply.github.com>
2023-07-03 12:47:08 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: "/src/main.jsx",
+      input: "src/main.jsx",
       external: ["uuid"],
     },
   },
