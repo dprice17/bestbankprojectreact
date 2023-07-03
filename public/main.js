7696dfb83f415bc7a72aa4e7ec745d232ceacb5c
dprice17 <109916870+dprice17@users.noreply.github.com>
2023-07-03 14:27:56 -0400

    update

--- public/main.js
+++ public/main.js
@@ -1,4 +1,14 @@
 import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<App />);
+
+/*
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -8,3 +18,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
+*/
